178d0322778969ef6ecab474aa51cb794add3238
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-05-15 19:58:48 +0530

    for loop

---
+++ js/frist.js
@@ -0,0 +1,132 @@
+// console.log("hello js ......")
+// let a=20;
+// let b=3;
+// console.log(a+b)
+ 
+// let r=3;
+// document.write(4*3.4*r*r)
+
+// let n1= parseInt (prompt("int num1"));
+// let n2= parseInt (prompt("int num2"));
+// document.write("sum of two num is " + (n1+n2))
+
+// wap to take input from user and chack num is even of odd
+// wap to take two input from user and check which is grater
+
+// let num = parseInt(prompt("Enter a number:"));
+// if (num % 2 == 0) {
+//    alert("num is even")
+// }else{
+//     alert("num is odd")
+// }
+
+// wap to take two input from user and check which is grater
+// let num1 = parseInt(prompt("Enter first num1:"));
+// let num2 = parseInt(prompt("Enter second num2:"));
+// if (num1 > num2) {
+//     alert(num1 + " is greater than " + num2);
+// }
+// else   {
+//     alert(num1 + " is less than " + num2);
+// }
+
+//wap to check num is positiv nigative and nutral
+
+
+// let num = parseInt(prompt("enter the num"));
+// if(num > 0) {
+//     alert( " num is positive");
+// }else if(num<0){
+//     alert("num is negative")
+// }else {
+//     alert("num is zero")
+// }
+
+// wap to print 1 to 10 digit using do while loop
+// let i = 1;
+
+// do{
+//     document.write(i);
+//     i++;
+// }while(i<=10)
+   
+
+
+// let i =1;
+ 
+// while(i>0){
+//     let num = parseInt(prompt("enter num"))
+
+//     if(num == 0){
+//         break;
+//     }
+
+//     document.write(num);
+//     i++;
+// }
+
+// do{
+//      var num = parseInt(prompt("enter num"))
+//      if( num == 0){
+//         break;
+//         }
+
+// }
+// while(1>0)
+
+
+
+//wap to print a tabel of n number 
+// let i =1;
+//  let n = parseInt(prompt("enter the number"))
+//  while(i<=10){
+//     document.write(n*i + "<br>");
+//   i++;
+//     }   
+
+      //  for loop
+
+//  for(let i=1; i<=10;i++){
+//   document.write(i + "<br>");
+//  }
+ 
+
+//  let n = parseInt(prompt("enter the num"))
+//  for(let i=1; i<=10;i++){
+//   document.write(n*i + "<br>");
+//  }
+
+//   for(let i=20; i>=10;i--){
+//    document.write( i + "<br>");
+//  }
+
+
+//  let n = parseInt(prompt("enter the num"))
+//  let a = n*n;
+//  for(let i=10; i>=1;i--){
+//   document.write(a*i + "<br>");
+//  }
+
+ //wap to print only even num betwen 1 to 15
+
+ 
+//  for(let i=1; i<=15;i++){
+//   if(i%2==0){
+//     document.write(i + "<br>")
+//   }
+  
+//  }
+ //wap to take one input from user and check it is even of odd if num is even then print the tabel of that num
+ //if num is odd then print cube of that num
+
+ let num =  parseInt(prompt("enter the num"))
+ 
+ if(num%2==0){
+  for(let i=1; i<=10;i++){
+    document.write(num*i + "<br>")
+  } 
+ }else {
+    let cube = num*num*num;
+    document.write(cube)
+   
+    }
