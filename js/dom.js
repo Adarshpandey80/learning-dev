990eff1d2033e0b1e81007c6e249605c61a1f11f
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-05-30 21:36:36 +0530

    DOM day2

--- js/dom.js
+++ js/dom.js
@@ -17,11 +17,22 @@
 
 // create ul list 10 list itom add even baground color blue and text color white
 
-let ul = document.getElementsByTagName("li");
-console.log(ul);
-for(let i =0 ;i<=10;i++){
-    if(i%2==0){
-        ul[i].style.backgroundColor = "blue"
-        ul[i].style.color = "white"
-    }
-}
+// let ul = document.getElementsByTagName("li");
+// console.log(ul);
+// for(let i =0 ;i<10;i++){
+//     if(i%2!=0){
+//         ul[i].style.backgroundColor = "blue"
+//         ul[i].style.color = "white"
+//     }
+// }
+
+// ===========================30/05/25====================
+
+let image = document.getElementById("image");
+image.src = "car2 copy.jpg"
+
+let head = document.getElementById("h1")
+head.setAttribute("align" , "center")  // use to apply the attributes
+
+let bd = document.body
+bd.removeAttribute("bgcolor")  // use to  remove the attributes
---
+++ js/domtask.js
@@ -0,0 +1,11 @@
+let img = document.getElementById("image")
+
+img.setAttribute("width" , "100%")
+
+let paira = document.getElementsByClassName("pir")
+  paira[0].style.backgroundColor = "yellow"
+paira[0].style.padding = "20px"
+paira[0].style.border = "2px solid black"
+
+
+
