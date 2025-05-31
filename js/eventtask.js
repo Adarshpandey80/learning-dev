2f69cd0d4e2a4542baa5a5718081437370fcd1c9
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-05-31 18:58:42 +0530

    Events

---
+++ js/eventtask.js
@@ -0,0 +1,12 @@
+function  fun (){
+    let a = parseInt(prompt("enter a number"))
+    let b = parseInt(prompt("enter a number"))
+    let c = a+b;
+    document.write(c)
+    
+}
+
+function fun1(a){
+    let b = document.body
+    b.style.backgroundColor = a;
+}
