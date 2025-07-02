6affde90d160ef3b6e0a4fb1f66e70569a5a9e3d
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-07-02 22:50:18 +0530

    JSON

--- js/JSONserver/app.js
+++ js/JSONserver/app.js
@@ -1,18 +1,38 @@
-// const express  = require("express")
-// const app = express()
-
-// app.get("/", (req, res) => {
-//     res.json({
-//         message: "Hello, World!"
-//     })
-// })
-
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000")
-// })
-
-// app.get("/api", (req, res) => { 
-//     res.send("API endpoint is working")
-// });
 
 
+let basurl = "http://localhost:3000/products";
+
+const getallproduct = async()=>{
+    try {
+        let res =await fetch(basurl);
+        let data = await res.json();
+        console.log(data)
+        let productDiv = document.getElementById("products");
+        productDiv.innerHTML=data.map((e)=>{
+            return `
+              <h1>id:${e.id}</h1>
+              <h1>id:${e.title}</h1>
+              <h1>id:${e.price}</h1>
+            `
+        })
+    } catch (e) {
+        alert(e)
+    }
+}
+
+const addproduct = async()=>{
+    let title = document.getElementById("title").value ;
+    let price = document.getElementById("price").value 
+
+    try {
+        await fetch(basurl , {
+           method :"POST",
+           headers:{
+            "Content -Type":"application/json"
+           },
+           body:json.stringify({title:title , price:price})
+        })
+    } catch (error) {
+         console.log(e)
+    }
+}
