0f79c80550b8cdb697a007e34a4d75ec19b81b8b
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-07-03 18:59:42 +0530

    Add and Update data POST,PUT

--- js/JSONserver/app.js
+++ js/JSONserver/app.js
@@ -11,16 +11,17 @@ const getallproduct = async()=>{
         productDiv.innerHTML=data.map((e)=>{
             return `
               <h1>id:${e.id}</h1>
-              <h1>id:${e.title}</h1>
-              <h1>id:${e.price}</h1>
+              <h1>title:${e.title}</h1>
+              <h1>price:${e.price}</h1>
             `
-        })
+        }).join('');
     } catch (e) {
         alert(e)
     }
 }
-
+// add product 
 const addproduct = async()=>{
+
     let title = document.getElementById("title").value ;
     let price = document.getElementById("price").value 
 
@@ -28,11 +29,36 @@ const addproduct = async()=>{
         await fetch(basurl , {
            method :"POST",
            headers:{
-            "Content -Type":"application/json"
+            "Content-Type":"application/json"
            },
-           body:json.stringify({title:title , price:price})
-        })
+           body:JSON.stringify({title:title , price:price})
+        });
+        getallproduct();
     } catch (error) {
-         console.log(e)
+         console.log(error);
+    }
+}
+
+//update 
+
+const updateproduct = async()=>{
+    let id = document.getElementById("id").value
+    let title = document.getElementById("utitle").value ;
+    let price = document.getElementById("uprice").value 
+
+    try {
+        await fetch( `${basurl}/${id}` , {
+           method :"PUT",
+           headers:{
+            "Content-Type":"application/json"
+           },
+           body:JSON.stringify({title:title , price:price})
+        });
+       alert("updated")
+       
+        getallproduct();
+       
+    } catch (error) {
+         console.log(error);
     }
 }
