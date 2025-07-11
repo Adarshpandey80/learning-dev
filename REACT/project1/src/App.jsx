fb41bc1125f83be4f9e7f871ae8650fdb565faae
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-07-11 19:07:41 +0530

    react component

--- REACT/project1/src/App.jsx
+++ REACT/project1/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const myfun = (e) => {
+    // alert(e.type)
+    //  let h= document.getElementById("h1")
+    // h.style.backgroundColor = "yellow"
+    // h.style.textAlign = "center"
+  }
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  
   return (
     <>
-     <h1>hello react</h1>
+      <h1 id="h1" onClick={myfun} style={{ color: "red", backgroundColor: "", cursor: "pointer" }}>hello react</h1>
+      {
+         arr.map((e)=>(
+          <h1>{e}</h1>
+         ))
+      }
+        
+      
     </>
   )
 }
---
+++ REACT/project1/src/Home.jsx
@@ -0,0 +1,7 @@
+function home (){
+    return (
+        <h1>home js file</h1>
+    )
+}
+
+export default home;
