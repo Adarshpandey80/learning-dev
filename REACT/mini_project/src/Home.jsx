a9bdcc185652a22604d191431af7eb5814432b3e
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-07-14 21:13:24 +0530

    Router

--- REACT/mini_project/src/Home.jsx
+++ REACT/mini_project/src/Home.jsx
@@ -4,7 +4,7 @@
    return (
      <div>
         <h1>this is home page</h1>
-        <img src="bikesimages/car1.jpg" alt="" height={"200px"} width={"200px"} />
+        <img src="./bikesimages/car1.jpg" alt="" height={"200px"} width={"200px"} />
      </div>
    )
  }
