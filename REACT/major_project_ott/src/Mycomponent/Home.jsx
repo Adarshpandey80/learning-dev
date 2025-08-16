2b94fc2b516f10273508365dc60a20f4ba971c17
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-08-16 21:30:13 +0530

    Remove mini_project

--- REACT/major_project_ott/src/Mycomponent/Home.jsx
+++ REACT/major_project_ott/src/Mycomponent/Home.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
-import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const Home = () => {
-  const location = useLocation( '/Login.jsx' );
+  const location = useLocation();
   const formdata = location.state || {};
   console.log("Form data from location state:", formdata);
 
   return (
     <>
     <div>
-      <h1>name : {formdata.name}</h1>
-      <h1>Email : {formdata.email}</h1>
-      <h1>Password : {formdata.password}</h1>
-         
-
-
+      {formdata && formdata.email && (
+        <div style={{ padding: '20px', backgroundColor: '#f0f0f0', marginBottom: '20px' }}>
+          <h1>Welcome User!</h1>
+          <h2>name : {formdata.namekey || formdata.name}</h2>
+          <h2>Email : {formdata.email}</h2>
+          <h2>Password : {formdata.password}</h2>
+        </div>
+      )}
     </div>
         <div className="bg-gradient-to-b from-[#0f172a] to-[#1e293b] text-white font-sans">
       {/* Hero Section */}
