899c488db594c57e3cc79a958a589d5d61121a11
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-10-07 19:27:10 +0530

    create modules userDefine and fs

---
+++ NodeJS/cybrom.js
@@ -0,0 +1,14 @@
+const cyberom=()=>{
+    return "<h1>this is cybrom class</h1>"
+}
+const subName =()=>{
+    return "<h2> learning MERN Stack </h2>"
+}
+const Fee = ()=>{
+    return "<h3>mt fees is 35000 </h3>"
+}
+module.exports = {
+    cyberom,
+    subName,
+    Fee
+}
--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,5 +1,39 @@
+// const http = require("http");
+// http.createServer((req,res)=>{
+// res.write("<h1> start node clases</h1>");
+// res.end("<h3> server end....</h3>");
+// }).listen(8000)
+
+// userDefine module use to use another function data
+
+// const http = require("http");
+// const cyb = require('./cybrom')
+// http.createServer((req,res)=>{
+//  res.write("<h1>create server</h1>")
+//   res.write(cyb.cyberom())
+//   res.write(cyb.subName());
+//   res.write(cyb.Fee());
+//  res.end()
+
+// }).listen(8000,()=>{
+//     console.log("server is listin on port 8080")
+// })
+ 
+
+
+// fs(File System ) module use to read data from another file
+
 const http = require("http");
+const fs = require('fs')
 http.createServer((req,res)=>{
-res.write("<h1> start node clases</h1>");
-res.end("<h3> server end....</h3>");
-}).listen(8000)
+ res.write("<h1>create server</h1>")
+   fs.readFile("newFile.txt", (err, data)=>{
+    if(err) throw err;
+    res.write(data)
+     res.end()
+   })
+
+
+}).listen(8000,()=>{
+    console.log("server is listin on port 8080")
+})
