284117d2efa8b3d8fa09dfee378f9664f78a1018
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-10-08 21:59:58 +0530

    CRUD  using fs module

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -23,17 +23,55 @@
 
 // fs(File System ) module use to read data from another file
 
-const http = require("http");
+// const http = require("http");
+// const fs = require('fs')
+// http.createServer((req,res)=>{
+//  res.write("<h1>create server</h1>")
+//    fs.readFile("newFile.txt", (err, data)=>{
+//     if(err) throw err;
+//     res.write(data)
+//      res.end()
+//    })
+
+
+// }).listen(8000,()=>{
+//     console.log("server is listin on port 8080")
+// })
+// ===============================================================08/10==========
+
+// in fs module create file
+
 const fs = require('fs')
-http.createServer((req,res)=>{
- res.write("<h1>create server</h1>")
-   fs.readFile("newFile.txt", (err, data)=>{
-    if(err) throw err;
-    res.write(data)
-     res.end()
-   })
 
+// fs.appendFile("ad.txt" , 'here i add some text data' , (err)=>{
+//   if(err) throw err;
+//   console.log("create new file");
+// })
 
-}).listen(8000,()=>{
-    console.log("server is listin on port 8080")
-})
+// fs.open() method
+
+// fs.open('new.txt' , 'w' ,(err ,file)=>{
+//   if(err) throw err;
+//   console.log("read file")
+// })
+
+// fs.writeFile() 
+
+// fs.writeFile('ad.txt' ,"new data updated" , (err)=>{
+//    if(err) throw err;
+//    console.log("update new data")
+// })
+
+// delete data fs.unlink()
+
+// fs.unlink('ad.txt' ,(err)=>{
+//   if(err) throw err
+//   console.log('delete file')
+// })
+
+// Rename file fs.rename()
+
+// fs.rename('newFile.txt' ,'new.txt' ,(err)=>{
+//   if(err) throw err;
+//   console.log("rename file");
+// })
