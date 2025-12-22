6428eba668612551f3047606fdbec9ad48a542ab
Adarsh Pandey <pandeyadarsh9628@gmail.com>
2025-12-22 22:36:36 +0530

    `Added react-icons package and updated task list functionality with edit and delete buttons`

--- REACT/Todo_redux/Todo_List/src/App.jsx
+++ REACT/Todo_redux/Todo_List/src/App.jsx
@@ -1,18 +1,30 @@
 import { useState } from "react"
-import {taskadd} from "./taskSlice"
-import {useSelector , useDispatch} from "react-redux"
-  
+import {taskadd, deletetask} from "./taskSlice"
+import {useSelector , useDispatch } from "react-redux"
+  import { MdDelete } from "react-icons/md";
+ 
 
 function App() {
   const [task , setTask] = useState("")
+  const [btn , setBtn] = useState(true);
   const dispatch = useDispatch();
   const mytask = useSelector(state=>state.mytask.task)
+
+
+  const edittask = (work)=>{
+      setTask(work);
+      setBtn(false)
+  }
 
  const alltask = mytask.map((key)=>{
        return (
         <>
         <tr>
               <td>{key.work}</td>
+              <td>  <button><MdDelete onClick={()=>{dispatch(deletetask( {id : key.id}))}} /> </button>  </td>
+               <rd> <button onClick={()=>{edittask(key.work)}}>Edit</button></rd>
+               
+              
          </tr>
         </>
              
@@ -22,13 +34,24 @@ function App() {
   return (
     <>
      <h1>this is doto list </h1>
-     add task : <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} /> <button onClick={()=>{dispatch(taskadd( {work : task}))}} >add task</button>
- 
+     add task : <input type="text" value={task} onChange={(e)=>setTask(e.target.value)} />
+
+     {
+      btn? (<> 
+        <button onClick={()=>{dispatch(taskadd( { id: Date.now() , work : task}))}} >add task</button>
+      </>):( <>
+             <button onClick={}>Update</button>
+      </>)
+     }
+     
+    
+
      <table>
      <tr>
       <th>
         work 
       </th>
+      <th></th>
      </tr>
        {alltask}
      </table>
--- REACT/Todo_redux/Todo_List/src/taskSlice.jsx
+++ REACT/Todo_redux/Todo_List/src/taskSlice.jsx
@@ -9,10 +9,15 @@ const taskSlice = createSlice({
         taskadd : (state, actions)=>  {
             console.log(actions.payload)
            state.task.push(actions.payload);
+        },
+        deletetask : (state , actions) => {
+            state.task = state.task.filter(item=>item.id !== actions.payload.id)
+        },
+        editform : (state ,actions)=>{
+            
         }
-
     }
 })
 
-export const {taskadd} = taskSlice.actions;
+export const {taskadd , deletetask, editform} = taskSlice.actions;
 export default taskSlice.reducer;
